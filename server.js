7028292827865fa60d339ab14521a84ba94c69a5
Shubham Rath <shubhamrath.iiitb@gmail.com>
2017-03-10 22:15:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); //to create the web server
+var morgan = require('morgan');   //to output log of our servers
 var path = require('path');
 
 var app = express();
