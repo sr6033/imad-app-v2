d9b7a80747f17f3fe9b9e0d14ed982da40dc54e3
Shubham Rath <shubhamrath.iiitb@gmail.com>
2017-03-12 01:29:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,8 @@ var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
-} 
+}
 
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
-}
+};
