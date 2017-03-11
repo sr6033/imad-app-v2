02f18dac3c6c8a0e21ecd5ee0c2bbd5bcf5e61c3
Shubham Rath <shubhamrath.iiitb@gmail.com>
2017-03-11 20:42:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,33 +63,33 @@ function createTemplate(data) {
 	var content = data.content;
 
 	var htmlTemplate = `
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>${title}</title>
-		<meta name="viewport" content="width-device-width, initial-scale=1"/>
-		<link href="/ui/style.css" rel="stylesheet" />
-	</head>
-
-	<body>
-		<div class="container">
-			<div>
-				<a href="/">Home</a>
-			</div>
-			<hr/>
-			<h3>
-				${heading}
-			</h3>
-			<div>
-				${date}
-			</div>
-			<div>
-				${content}
-			</div>
-		</div>
-	</body>
-	</html>
-	`;
+    	<!DOCTYPE html>
+    	<html>
+    	<head>
+    		<title>${title}</title>
+    		<meta name="viewport" content="width-device-width, initial-scale=1"/>
+    		<link href="/ui/style.css" rel="stylesheet" />
+    	</head>
+    
+    	<body>
+    		<div class="container">
+    			<div>
+    				<a href="/">Home</a>
+    			</div>
+    			<hr/>
+    			<h3>
+    				${heading}
+    			</h3>
+    			<div>
+    				${date}
+    			</div>
+    			<div>
+    				${content}
+    			</div>
+    		</div>
+    	</body>
+    	</html>
+    	`;
 	return htmlTemplate;
 }
 
@@ -100,7 +100,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
-    var articleName = req.param.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
